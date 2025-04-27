d2a71d018cc9a4aef27cd7a81f709e3559d95659
tamaqazaq <yermukhanovdaulet@gmail.com>
2025-04-27 20:05:32 +0500

    Fix: CORS

--- cmd/main.go
+++ cmd/main.go
@@ -40,7 +40,7 @@ func main() {
 
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://linkup.pages.dev"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
