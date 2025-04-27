8bf817c091ae8cd271de915eaf8a1fd331c40603
tamaqazaq <yermukhanovdaulet@gmail.com>
2025-04-27 20:49:38 +0500

    fix: use dynamic port for Railway

--- cmd/main.go
+++ cmd/main.go
@@ -88,5 +88,10 @@ func main() {
 		likeRoutes.GET("/user/:user_id", likeHandler.GetLikesByUser)
 	}
 
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port)
+
 }
